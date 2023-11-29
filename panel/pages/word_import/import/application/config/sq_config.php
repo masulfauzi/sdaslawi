c9001a60987db3a65203e44a0d7a647a0ad70a80
masulfauzi <masul.fauzi@gmail.com>
2023-11-29 20:55:21 +0700

    initial commit

---
+++ panel/pages/word_import/import/application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php
+//include "../../../../config/config.database.php";
+$sq_base_url = '';
+$sq_hostname = 'localhost';
+$sq_dbname = 'sdaslawi_candyold';
+$sq_dbusername = 'sdaslawi_usrcandyold';
+$sq_dbpassword = 'Xz]+fEEbYi!z';
